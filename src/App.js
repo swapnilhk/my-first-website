15a8121ac21e67c8f43cd52c49629cb6d1e72069
Swapnil Kharabe <swapnilhk@gmail.com>
2020-09-18 15:00:43 +0530

    Website look and feel basics done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import data from "./Data"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      level : 0
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
+  
+  handleClick(incr){
+    let newLevel = this.state.level + incr //Increment level
+    console.log("newlve: " + newLevel)
+    this.setState((state, props) => {
+    if(newLevel >= 0 && newLevel < data.length) {
+      return {
+        level : newLevel
+      }
+    }
+  })}
+
+  render (){
+    return(
+      <React.Fragment >
+        <div className="container">
+        <div className="subcontainer1">
+            <p>{data[this.state.level].text}</p>
+          </div>
+          <div className="subcontainer2">
+            {this.getContent(data[this.state.level])}
+          </div>
+          <button class="prev" onClick={() => this.handleClick(-1)}>Prev</button>
+          <button class="next" onClick={() => this.handleClick(+1)}>Next</button>
+        </div>
+      </React.Fragment>
+    )
+  }
+
+  getContent(currData){
+    if(currData.img != null)
+      return <img src={currData.img} alt=""/>
+    else if(currData.options != null){
+      return currData.options.map((option) => 
+        <React.Fragment >
+          <input type="radio" class="radio" name="answer" value="{option}" onChange=""/><label>{option}</label><br/>  
+        </React.Fragment>
+      )}
+  }
 }
 
-export default App;
+export default App; 
---
+++ src/Data.js
@@ -0,0 +1,26 @@
+const data = [
+    {
+        text: "Do you thing it is right?",
+        options: [
+            "Yes",
+            "No",
+            "Maybe",
+            "Don't know"
+        ]
+    },
+    {
+        img : "images/comica1579314127970.jpg",
+        text : "Colors!"
+    },
+    {
+        img: "images/DSC_0314.JPG",
+        text : "Pose!"
+    },
+    {
+        img: "images/IMG_20180818_113241.jpg",
+        text: "Babe!"
+    }
+    
+]
+
+export default data
