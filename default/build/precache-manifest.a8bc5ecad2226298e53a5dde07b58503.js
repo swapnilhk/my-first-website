229183de57551daf57796ff2810243149879c5e0
Swapnil Kharabe <swapnilhk@gmail.com>
2020-09-21 15:08:31 +0530

    restructure

---
+++ default/build/precache-manifest.a8bc5ecad2226298e53a5dde07b58503.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fbf8e19ae94af8f63efb2ca2f732dd4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3bb9e2ffc40cce57c66d",
+    "url": "/static/css/main.f334a2cd.chunk.css"
+  },
+  {
+    "revision": "610835c6bf1db40ccc15",
+    "url": "/static/js/2.0f4ee34a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0f4ee34a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bb9e2ffc40cce57c66d",
+    "url": "/static/js/main.402e9692.chunk.js"
+  },
+  {
+    "revision": "e8900eab1df2781d1a7b",
+    "url": "/static/js/runtime-main.f3683586.js"
+  }
+]);
